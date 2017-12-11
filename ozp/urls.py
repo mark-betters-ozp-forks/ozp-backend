8e9a35139fe0174e8e2aa69bb56f6f9912b15195
mbetters <Betters_Mark@bah.com>
2017-12-11 13:04:28 -0500

    Patched in Damon's updates

--- ozp/urls.py
+++ ozp/urls.py
@@ -23,15 +23,15 @@ from ozp.decorators.cas_decorators import redirecting_login_required
 from decorator_include import decorator_include
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', decorator_include(redirecting_login_required, 'ozpcenter.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^iwc-api/', include('ozpiwc.urls')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'admin/', include(admin.site.urls)),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'api/', decorator_include(redirecting_login_required, 'ozpcenter.urls')),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'iwc-api/', include('ozpiwc.urls')),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'docs/', include('rest_framework_swagger.urls')),
 
     # CAS
-    url(r'^accounts/login/$', 'cas.views.login', name='login'),
-    url(r'^accounts/logout/$', 'cas.views.logout', name='logout'),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'accounts/login/$', 'cas.views.login', name='login'),
+    url(r'^' + settings.OZP['ROOT_URL'] + 'accounts/logout/$', 'cas.views.logout', name='logout'),
 ]
 
 # in debug, serve the media and static resources with the django web server
